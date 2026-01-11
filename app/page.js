4f4b9510e66f9a0f0c79aca604170b3583560022
sharmasamdeesh86-prog <sharmasamdeesh86@gmail.com>
2026-01-11 17:49:44 +0000

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>🎵 Video Audio Recogniser</h1>
-      <p>Your app is ready for Vercel.</p>
-      <p>Next step: add audio recognition.</p>
-    </main>
-  );
-}
