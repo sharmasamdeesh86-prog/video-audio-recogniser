17ec0ea58874ade10e222003a3ddc0799ad31b63
sharmasamdeesh86-prog <sharmasamdeesh86@gmail.com>
2026-01-11 18:04:56 +0000

    Delete app/app/page.js

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>🎵 Video Audio Recogniser</h1>
-      <p>Your app is now correctly structured.</p>
-    </main>
-  );
-}
