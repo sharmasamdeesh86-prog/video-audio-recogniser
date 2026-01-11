a855dc2527606680e174335e4b522cbe620cdab7
sharmasamdeesh86-prog <sharmasamdeesh86@gmail.com>
2026-01-11 18:03:43 +0000

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>🎵 Video Audio Recogniser</h1>
+      <p>Your app is now correctly structured.</p>
+    </main>
+  );
+}
